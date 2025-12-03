52adcd257facd4b01c62d68a69793f8af396a8e3
unknown <trunghauvoo3105@gmail.com>
2025-12-03 11:53:08 +0700

    thêm lesson-02.2 và các file liên quan

---
+++ tests/lesson-02.2/ex1.js
@@ -0,0 +1,7 @@
+const number = 13;
+var name = "my number";
+var isEven =false;
+if(number % 2 ==0){
+    isEven = true;
+    console.log(isEven)
+}
---
+++ tests/lesson-02.2/ex3.js
@@ -0,0 +1,8 @@
+const number = 0;
+if(number > 0){
+    console.log("Gía trị bạn nhập là số dương");
+} else if(number < 0){
+    console.log("Gía trị bạn nhập là số âm");
+}else if(number === 0){
+    console.log("Gía trị bạn nhập là số 0")
+};
